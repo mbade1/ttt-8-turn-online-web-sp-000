445c3fc355f35560d82a895281d873fa5610f694
Michael Bade <mbade314@gmail.com>
2020-05-08 14:27:25 +0000

    Automatically backed up by Learn

---
+++ lib/turn.rb
@@ -0,0 +1,21 @@
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(user_input)
+  index = user_input.to_i
+  return index - 1
+end
+
+def valid_move?(board, index)
+  if index.between?(0, 8) && !position_taken(board, index)
+    return true
+  end
+  return false
+end
+
+def position_taken
